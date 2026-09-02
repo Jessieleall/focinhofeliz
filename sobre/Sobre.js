a4a20dc34e0f66c49fe275f50f767be8e1ec21ca
Jessieleall <jessicasantosleal7@gmail.com>
2026-09-02 20:31:36 -0300

    Add files via upload

---
+++ sobre/Sobre.js
@@ -0,0 +1,9 @@
+// 1) Seleciona os elementos que vamos usar
+const botaoMenu = document.getElementById("botao-menu");
+const menu = document.querySelector("nav");
+ 
+// 2) Escuta o evento de clique no botão
+// 3) Reage alternando a classe "menu-aberto" no <nav>
+botaoMenu.addEventListener("click", function () {
+  menu.classList.toggle("menu-aberto");
+});
